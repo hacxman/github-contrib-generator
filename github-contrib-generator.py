534c0e25fd23df4e5e9814ff6c7f0cf4952487a9
Maros Zatko <hacxman@gmail.com>
2013-11-19 01:51:40 +0100

    timezone correction

--- github-contrib-generator.py
+++ github-contrib-generator.py
@@ -2,14 +2,14 @@
 import os
 import sys
 import time
-from datetime import datetime
+import datetime
 from datetime import timedelta
 
 def stringdate(days):
-  d = datetime.today()
+  d = datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=-8)))
   dlt = timedelta(days=days)
   ntajm = (d-dlt)
-  return time.asctime(ntajm.timetuple()), datetime.isoweekday(ntajm)
+  return time.asctime(ntajm.timetuple()), datetime.datetime.isoweekday(ntajm)
 
 if __name__ == "__main__":
   if len(sys.argv) < 3:
@@ -20,9 +20,9 @@ if __name__ == "__main__":
   repo  = sys.argv[2]
   with open(fname) as fin:
     data = fin.readlines()
-  print(data)
 
   seen_sunday = False
+  j = 0
   for i in range(365, 0, -1):
     dejt, dej = stringdate(i)
 
@@ -30,11 +30,16 @@ if __name__ == "__main__":
       continue
     seen_sunday = True
 
-    cnt = int(data[(365-i) % 7][(365-i) // 7])
-    print(cnt, i, (365-i) % 7, (365-i) // 7, dej, dejt)
+    cnt = int(data[j % 7][j // 7])
+#    print(cnt, i, j % 7, j // 7, dej, dejt)
+    sys.stdout.write("{}%    \r".format(int(100*(365-i)/364.0)))
     for a in range(cnt):
       with open(os.path.join(repo, 'lulz'), "a") as fout:
         fout.write("{} {}\n".format(dejt, a))
       os.system("pushd \"{}\" > /dev/null ; git add lulz ;\
-         git commit --date \"{}\" -mlol > /dev/null ; popd > /dev/null"
-          .format(repo, dejt))
+         git commit --date \"{} -0800\" -mlol > /dev/null ; popd > /dev/null"
+          .format(repo, dejt)) # ^^^^ - Timezone offset
+
+    j += 1
+
+  print("done")
